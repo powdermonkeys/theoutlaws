cefb10b0b59f4c6486f0acee76a2c20a3ec4a200
Roopal Kondepudi <roopalk@fb.com>
2018-07-17 16:25:46 -0700

    temp...will move this to when the attraction is clicked

--- Voyager/app/src/main/java/com/example/roopalk/voyager/LoginActivity.java
+++ Voyager/app/src/main/java/com/example/roopalk/voyager/LoginActivity.java
@@ -1,19 +1,29 @@
 package com.example.roopalk.voyager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.roopalk.voyager.Fragments.FragmentAdapter;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn) Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
 
         // Get the ViewPager and set it's PagerAdapter so that it can display items
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
@@ -23,6 +33,15 @@ public class LoginActivity extends AppCompatActivity {
         // Give the TabLayout the ViewPager
         TabLayout tabLayout = findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, PhotoUpload.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 
