b08513d30a364046b6a66e071fdd11a764f70686
Roopal Kondepudi <roopalk@fb.com>
2018-07-18 11:25:41 -0700

    glide

--- Voyager/app/src/main/java/com/example/roopalk/voyager/Fragments/BuildFragment.java
+++ Voyager/app/src/main/java/com/example/roopalk/voyager/Fragments/BuildFragment.java
@@ -3,7 +3,10 @@ package com.example.roopalk.voyager.Fragments;
 import android.app.DialogFragment;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 public class  BuildFragment extends Fragment {
     @Override
@@ -11,7 +14,20 @@ public class  BuildFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    public void showDatePickerDialog(View v) {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState)
+    {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    public void showDatePickerDialog(View v)
+    {
         DialogFragment newFragment = new DatePickerFragment();
         newFragment.show(getFragmentManager(), "datePicker");
     }
