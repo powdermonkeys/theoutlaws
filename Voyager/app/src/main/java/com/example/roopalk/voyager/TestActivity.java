fd62727100eff3c47545b3daebcf655838d428d1
Roopal Kondepudi <roopalk@fb.com>
2018-07-19 11:08:15 -0700

    going to change into NetworkUtilityClass.java

--- Voyager/app/src/main/java/com/example/roopalk/voyager/TestActivity.java
+++ Voyager/app/src/main/java/com/example/roopalk/voyager/TestActivity.java
@@ -6,11 +6,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.roopalk.voyager.Model.Photo;
-import com.parse.FindCallback;
-import com.parse.ParseException;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,28 +25,5 @@ public class TestActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         ButterKnife.bind(this);
-
-        final Photo.Query photoQuery = new Photo.Query();
-
-        photoQuery.getAllImages();
-        photoQuery.findInBackground(new FindCallback<Photo>()
-        {
-            @Override
-            public void done(List<Photo> objects, ParseException e)
-            {
-                if(e == null)
-                {
-                    photos.add(objects.get(0));
-
-                    Photo photo = photos.get(0);
-
-                    String imagePath = photo.getImage().getUrl();
-
-                    GlideApp.with(getBaseContext())
-                            .load(imagePath)
-                            .into(ivGottenPhoto);
-                }
-            }
-        });
     }
 }
