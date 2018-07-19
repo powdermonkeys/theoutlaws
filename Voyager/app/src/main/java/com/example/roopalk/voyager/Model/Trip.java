9be9621b37fa2164652c8f95cf38e1637490c206
Roopal Kondepudi <roopalk@fb.com>
2018-07-19 11:08:03 -0700

    made it into a parse class

--- Voyager/app/src/main/java/com/example/roopalk/voyager/Model/Trip.java
+++ Voyager/app/src/main/java/com/example/roopalk/voyager/Model/Trip.java
@@ -1,26 +1,74 @@
 package com.example.roopalk.voyager.Model;
 
 import com.parse.FindCallback;
+import com.parse.ParseClassName;
 import com.parse.ParseException;
+import com.parse.ParseObject;
 
 import java.util.List;
 
-public class Trip
+@ParseClassName("Trip")
+public class Trip extends ParseObject
 {
-    public static int NUM_GUESTS;
-    public static String CHECK_IN;
-    public static String CHECK_OUT;
-    public static City CITY_DEST;
 
-    public Trip(int num_guests, String check_in, String check_out, String destination, String country)
+    //in the Parse database, the number of guests is stored in a column called guests
+    private static final String NUM_GUESTS = "guests";
+
+    //the checkin date of the trip stored in a column called checkin
+    private static final String CHECKIN = "checkin";
+
+    //the checkout date of the trip is stored in a column called checkout
+    private static final String CHECKOUT = "checkout";
+
+    //the destination of the trip is stored in a column called destination
+    private static final String DESTINATION = "destination";
+
+    private static String city = "";
+    private static String country = "";
+
+    public Trip(String ct, String tree)
     {
-        NUM_GUESTS = num_guests;
-        CHECK_IN = check_in;
-        CHECK_OUT = check_out;
-        getDestination(destination, country);
+        city = ct;
+        country = tree;
+        setDestination(city, country);
     }
 
-    private void getDestination(String destination, String country)
+    //getter methods for each of the values
+
+    public int getNumGuests()
+    {
+        return getInt(NUM_GUESTS);
+    }
+
+    public String getCheckin() {
+        return getString(CHECKIN);
+    }
+
+    public String getCheckout()
+    {
+        return getString(CHECKOUT);
+    }
+
+    public String getDestination()
+    {
+        return getString(DESTINATION);
+    }
+
+    public void setNumGuests(int numguests)
+    {
+        put(NUM_GUESTS, numguests);
+    }
+
+    public void setCheckin(String checkin) {
+        put(CHECKIN, checkin);
+    }
+
+    public void setCheckout(String checkout)
+    {
+        put(CHECKOUT, checkout);
+    }
+
+    private void setDestination(String destination, String country)
     {
         final City.Query cityQuery = new City.Query();
 
@@ -35,7 +83,7 @@ public class Trip
                 if(e == null)
                 {
                     //get the first object that is found that is name with this city
-                    CITY_DEST = objects.get(0);
+                    put(DESTINATION, objects.get(0));
                 }
             }
         });
