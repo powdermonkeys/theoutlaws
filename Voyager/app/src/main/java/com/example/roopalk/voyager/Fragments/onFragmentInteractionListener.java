0d333e023ce0dd355bfeebb7172373eca7aa3f2e
Roopal Kondepudi <roopalk@fb.com>
2018-07-23 18:06:53 -0700

    interface for all listener commands

---
+++ Voyager/app/src/main/java/com/example/roopalk/voyager/Fragments/onFragmentInteractionListener.java
@@ -0,0 +1,8 @@
+package com.example.roopalk.voyager.Fragments;
+
+import com.example.roopalk.voyager.Model.Attraction;
+
+public interface onFragmentInteractionListener
+{
+    public void moveToDetailsPage(Attraction attraction);
+}
