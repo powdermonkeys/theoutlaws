f5b64f56560e1900ccc70c3e8c4cca300d08a23b
Selome Zerai <selzerai@fb.com>
2018-07-26 16:06:14 -0700

    changing how the calendar looks and spacing of trip builder

--- Voyager/app/src/main/java/com/example/roopalk/voyager/Fragments/CalendarFragment.java
+++ Voyager/app/src/main/java/com/example/roopalk/voyager/Fragments/CalendarFragment.java
@@ -43,6 +43,12 @@ public class CalendarFragment extends Fragment {
         return fragment;
     }
 
+    static {
+        events.add(new Event("Tester 1", 3f, 2f, 10, 30));
+        events.add(new Event("Tester 2", 4f, 9f, 20, 60));
+        events.add(new Event("Tester 3", 7f, 4f, 30, 90));
+
+    }
 
 
     @Override
