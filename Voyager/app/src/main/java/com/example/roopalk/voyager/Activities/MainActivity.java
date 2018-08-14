29be44378a9e8a44dcdf149f6c0b79761fb3df2e
Roopal Kondepudi <roopalk@fb.com>
2018-08-14 09:33:25 -0700

    added Splash Screen

--- Voyager/app/src/main/java/com/example/roopalk/voyager/Activities/MainActivity.java
+++ Voyager/app/src/main/java/com/example/roopalk/voyager/Activities/MainActivity.java
@@ -51,6 +51,7 @@ public class  MainActivity extends AppCompatActivity implements onFragmentIntera
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
